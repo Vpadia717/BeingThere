1dd4b33cd25d3307fb7b4647bfcd2a800774dd5e
vivek <vpadia717@rku.ac.in>
2022-03-29 20:00:17 +0530

    Commit

---
+++ app/src/main/java/com/example/beingthere/SignUp.java
@@ -0,0 +1,84 @@
+package com.example.beingthere;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SignUp extends AppCompatActivity {
+
+
+    //Variables
+    ImageView backBtn;
+    Button next, login;
+    TextView titleText, slideText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retailer_sign_up);
+
+
+        //Hooks for animation
+        backBtn = findViewById(R.id.signup_back_button);
+        next = findViewById(R.id.signup_next_button);
+        login = findViewById(R.id.signup_login_button);
+        titleText = findViewById(R.id.signup_title_text);
+        slideText = findViewById(R.id.signup_slide_text);
+
+
+    }
+
+
+    /*public void callNextSigupScreen(View view) {
+
+
+        Intent intent = new Intent(getApplicationContext(), SignUp2ndClass.class);
+
+
+        //Add Shared Animation
+        Pair[] pairs = new Pair[5];
+        pairs[0] = new Pair<View, String>(backBtn, "transition_back_arrow_btn");
+        pairs[1] = new Pair<View, String>(next, "transition_next_btn");
+        pairs[2] = new Pair<View, String>(login, "transition_login_btn");
+        pairs[3] = new Pair<View, String>(titleText, "transition_title_text");
+        pairs[4] = new Pair<View, String>(slideText, "transition_slide_text");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
+
+
+    }*/
+
+    public void callSignUpScreen(View view){
+        Intent intent = new Intent(getApplicationContext(),Home.class);
+
+        Pair[] pairs = new Pair[1];
+
+        pairs[0] = new Pair<View,String>(findViewById(R.id.signup_back_button),"transition_signup");
+
+        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SignUp.this,pairs);
+        startActivity(intent, activityOptions.toBundle());
+        finish();
+
+    }
+
+    public void callLoginFromSignUp(View view) {
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+
+
+}
